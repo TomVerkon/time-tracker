be85c2cface2343b388a0b6a2e700d782f15fcc3
Douglass Hooker <DouglassHooker@gmail.com>
2018-04-01 19:31:04 -0500

    Update eslint and correct some errors

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Router, Route } from 'react-router';
+import { Router, Route } from 'react-router'
 
 import Home from './components/Home'
 import Login from './components/Login'
@@ -7,11 +7,9 @@ import Signup from './components/Signup'
 import TimerPage from './components/TimerPage'
 
 
-const { ipcRenderer } = window.require('electron')
-
 const App = (props) => {
   return (
-    <div style={{ background: "#FEFEFE", border: '5px solid #5755d9', height: '100vh', textAlign: 'center', boxShadow: 'inset 0 0 5px 0px black' }}>
+    <div style={{ background: '#FEFEFE', border: '5px solid #5755d9', height: '100vh', textAlign: 'center', boxShadow: 'inset 0 0 5px 0px black' }}>
       <Router history={props.history}>
         <div>
           <Route path="/" exact component={Home} />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,28 +3,27 @@ import { Link } from 'react-router-dom'
 
 const { shell } = require('electron')
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div>
-        <h2 style={{ marginTop: 0, paddingTop: 25 }}>Time Tracker</h2>
-        <p>The simple time tracking app</p>
-        <div style={{ width: '50%', margin: '0 auto', display: 'flex', justifyContent: 'space-between' }}>
-          <Link className="btn" to="/signup">Signup</Link>
-          <Link className="btn btn-primary" to="/login">Login</Link>
-        </div>
-        <div style={{ width: '50%', margin: '0 auto', marginTop: '10px' }}>
-          <Link style={{ width: '100%' }} className="btn btn-error" to="/timers" >Continue offline</Link>
-        </div>
-        <div style={{ marginTop: '20px' }}>
-          <a onClick={(e) => {
-            e.preventDefault()
-            shell.openExternal('https://github.com/DukeFerdinand/hour-tracker-electron')
-          }}>View Source Code</a>
-        </div>
+const Home = () => {
+  return (
+    <div>
+      <h2 style={{ marginTop: 0, paddingTop: 25 }}>Time Tracker</h2>
+      <p>The simple time tracking app</p>
+      <div style={{ width: '50%', margin: '0 auto', display: 'flex', justifyContent: 'space-between' }}>
+        <Link className="btn" to="/signup">Signup</Link>
+        <Link className="btn btn-primary" to="/login">Login</Link>
       </div>
-    )
-  }
+      <div style={{ width: '50%', margin: '0 auto', marginTop: '10px' }}>
+        <Link style={{ width: '100%' }} className="btn btn-error" to="/timers" >Continue offline</Link>
+      </div>
+      <div style={{ marginTop: '20px' }}>
+        <a role="navigation" onClick={(e) => {
+          e.preventDefault()
+          shell.openExternal('https://github.com/DukeFerdinand/hour-tracker-electron')
+        }}
+        >View Source Code</a>
+      </div>
+    </div>
+  )
 }
 
 export default Home
