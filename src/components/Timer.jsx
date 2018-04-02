27fe9024bca23fe3af85d93454d5b3627267d8d7
= <DouglassHooker@gmail.com>
2018-04-02 00:30:26 -0500

    Clean up timer rendering
    Extracts timer 'stringification' to separate functions
    
    Adds nice styling for timers with tentative dropdown

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -4,7 +4,7 @@ export default class Timer extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      seconds: 0,
+      seconds: 55,
       minutes: 0,
       hours: 0,
       timerActive: false
@@ -19,20 +19,24 @@ export default class Timer extends React.Component {
       ws.send('connection')
     }
 
-    // event emmited when receiving message 
+    // event emmited when receiving message
     ws.onmessage = (res) => {
       ws.send('pong')
     }
+    this.setTimerFromProps()
     this.timerHandler()
   }
+  setTimerFromProps() {
+    const { value } = this.props.timer
+    this.setState({
+      hours: value.hours,
+      minutes: value.minutes,
+      seconds: value.seconds
+    })
+  }
   timerHandler() {
-    let { timer } = this.props
-    if (timer) {
-      const splitTimes = timer.value.split(':')
-      console.log(splitTimes)
-    }
-    let interval = setInterval(() => {
-      if(this.state.timerActive) {
+    setInterval(() => {
+      if (this.state.timerActive) {
         if (this.state.seconds++ < 59) {
           // Setting Seconds
           this.setState({
@@ -59,18 +63,43 @@ export default class Timer extends React.Component {
       }
     }, 1000)
   }
-  render() {
+  stringifyTime() {
     const hours = (this.state.hours.toString().length === 1 ? `0${this.state.hours}` : this.state.hours)
     const minutes = (this.state.minutes.toString().length === 1 ? `0${this.state.minutes}` : this.state.minutes)
     const seconds = (this.state.seconds.toString().length === 1 ? `0${this.state.seconds}` : this.state.seconds)
+    if (this.state.hours !== 0) {
+      return {
+        time: `${hours}:${minutes}`,
+        tooltip: 'Hours : Minutes'
+      }
+    }
+    return {
+      time: `${minutes}:${seconds}`,
+      tooltip: 'Minutes : Seconds'
+    }
+  }
+  render() {
+    const timer = this.stringifyTime()
+    console.log(timer)
+    console.log(this.state)
     return (
-      <div style={{ textAlign: 'left', padding: '5px' }}>
-        <div className="tile tile-centered">
+      <div style={{ textAlign: 'left', padding: '5px', marginBottom: '15px' }}>
+        <div className="tile">
           <div className="tile-content">
-            <h5 className="tile-title">{hours != 0 ? `${hours}:${minutes}` : `${minutes}:${seconds}`} - {this.props.name || 'Timer'}</h5>
-            <div className="tile-subtitle text-gray">14MB · Public · 1 Jan, 2017</div>
+            <h5 style={{ zIndex: `${this.props.index}` }} className="tile-title tooltip tooltip-bottom" data-tooltip={timer.tooltip}>{timer.time}</h5>
+            <div className="tile-subtitle text-gray">{this.props.timer.project}</div>
           </div>
           <div className="tile-action">
+            <div className="dropdown dropdown-right">
+              <span className="btn btn-link dropdown-toggle" tabIndex="0">
+                <i className="icon icon-caret" />
+              </span>
+              <ul style={{ marginBottom: '25px' }} className="menu">
+                <li style={{ cursor: 'pointer' }}>Test</li>
+                <li className="divider" />
+                <li style={{ cursor: 'pointer' }} className="text-error">Remove</li>
+              </ul>
+            </div>
             <button className="btn btn-error" onClick={() => this.setState({ timerActive: !this.state.timerActive })}>{this.state.timerActive ? 'Stop' : 'Start'}</button>
           </div>
         </div>
